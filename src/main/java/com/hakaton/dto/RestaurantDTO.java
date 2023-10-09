3d6e810b8632415fd29e4a1dde9317daec46ceae
Ivan Skalyga <vanya729@ukr.net>
2023-10-09 20:06:10 +0300

    dto1

--- src/main/java/com/hakaton/dto/RestaurantDTO.java
+++ src/main/java/com/hakaton/dto/RestaurantDTO.java
@@ -3,10 +3,12 @@ package com.hakaton.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Accessors(chain = true)
 public class RestaurantDTO {
         private String city;
         private String name;
